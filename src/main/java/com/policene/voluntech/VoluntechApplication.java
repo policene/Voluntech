b97719ffd34189590747695e8cb48da586639a96
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-04 10:40:15 -0300

    initial commit

---
+++ src/main/java/com/policene/voluntech/VoluntechApplication.java
@@ -0,0 +1,13 @@
+package com.policene.voluntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntechApplication.class, args);
+	}
+
+}
