f4f1e8135024611fa2bee240ca1214edb8450779
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-04 21:59:13 -0300

    feat: Get All Volunteers

--- src/main/java/com/policene/voluntech/controllers/VolunteerController.java
+++ src/main/java/com/policene/voluntech/controllers/VolunteerController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/volunteers")
 public class VolunteerController {
@@ -19,6 +22,11 @@ public class VolunteerController {
         this.volunteerService = volunteerService;
     }
 
+    @GetMapping("/get")
+    public ResponseEntity<List<VolunteerResponseDTO>> getAllVolunteers() {
+        List<VolunteerResponseDTO> volunteers = volunteerService.getAll().stream().map(VolunteerResponseDTO::new).collect(Collectors.toList());
+        return new ResponseEntity<>(volunteers, HttpStatus.OK);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerVolunteer(@RequestBody @Valid VolunteerRequestDTO request) {
--- src/main/java/com/policene/voluntech/services/VolunteerService.java
+++ src/main/java/com/policene/voluntech/services/VolunteerService.java
@@ -1,11 +1,14 @@
 package com.policene.voluntech.services;
 
 import com.policene.voluntech.dtos.VolunteerRequestDTO;
+import com.policene.voluntech.dtos.VolunteerResponseDTO;
 import com.policene.voluntech.models.entities.Volunteer;
 import com.policene.voluntech.models.enums.UserRole;
 import com.policene.voluntech.repositories.VolunteerRepository;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VolunteerService {
@@ -23,5 +26,7 @@ public class VolunteerService {
     }
 
 
-
+    public List<Volunteer> getAll() {
+        return repository.findAll();
+    }
 }
