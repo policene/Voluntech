e4f5f028bbc33f8316a5535378f504e2fa397cd5
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-19 15:06:01 -0300

    build: removed OAuth2, added OpenAPI/Swagger

--- src/main/java/com/policene/voluntech/config/security/SecurityConfig.java
+++ src/main/java/com/policene/voluntech/config/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -32,7 +33,6 @@ public class SecurityConfig {
                 // Desabilita o CSRF, porque vamos utilizar o token JWT.
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
-                .formLogin(Customizer.withDefaults())
                 // Define rotas públicas que não necessitam de autenticação (register e login).
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
@@ -42,12 +42,26 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
-                .oauth2Login(Customizer.withDefaults())
                 // Define a política de sessões como stateless.
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // O filtro para validação do token é executado antes de qualquer outro padrão do Spring Security.
                 .build();
 
+    }
+
+
+    // Função que faz com que as seguintes URLs sequer passem pelo filtro de segurança.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+            web.ignoring().requestMatchers(
+                    "/v2/api-docs/**",
+                    "/v3/api-docs/**",
+                    "/swagger-resources/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/webjars/**"
+            );
     }
 
     @Bean
